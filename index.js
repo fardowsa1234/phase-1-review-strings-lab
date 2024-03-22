a6af5a4968d08ba72f21a101d950c7965ab89b53
fardowsa123 <--fardowsa.abdullahi@student.moringaschool.com>
2024-03-22 12:37:22 +0300

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Write your code in this file!
+// Define the currentUser variable
+const currentUser = "John";
+
+// Define the welcomeMessage variable
+const welcomeMessage = "Welcome to Flatbook, " + currentUser + "!";
+
+// Define the excitedWelcomeMessage variable
+const excitedWelcomeMessage = "WELCOME TO FLATBOOK, " + currentUser.toUpperCase() + "!";
+
+// Define the shortGreeting variable
+const shortGreeting = "Welcome, " + currentUser[0] + "!";
+
